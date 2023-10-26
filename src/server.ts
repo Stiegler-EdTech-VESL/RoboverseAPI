37332823f985233d61413775b6d8064c72fd654a
Erin Woodard <erin@stiegleredtech.org>
2023-10-26 15:43:44 -0400

    changed property name for User

--- src/server.ts
+++ src/server.ts
@@ -303,7 +303,7 @@ app.post(
         global_sigma: undefined,
         global_ranking: undefined,
         global_rank_title: undefined,
-        current_eq: undefined,
+        current_eq_id: undefined,
       });
       res.json({ message: "Player Created", user_id: user.id });
     } catch (error) {
@@ -334,7 +334,7 @@ app.put(
         totalEqMatches: undefined,
         totalEqMatchesWon: undefined,
         totalEqMatchesLost: undefined,
-        current_eq: (req.params.id as string) || undefined,
+        current_eq_id: (req.params.current_eq_id as string) || undefined,
       });
       res.json({ message: "Player Updated", user_id: user.id });
     } catch (error) {
