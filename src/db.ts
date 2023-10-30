8139ed7ed40ee3259090f989fc9f55d8a899f02d
Erin Woodard <erin@stiegleredtech.org>
2023-10-30 17:26:54 -0400

    fix for upsertUser

--- src/db.ts
+++ src/db.ts
@@ -193,7 +193,7 @@ export async function upsertUser(data: User) {
         current_eq_id: data.current_eq_id || undefined,
       },
       create: {
-        name: data.name || undefined,
+        name: "poop",
         email: data.email || undefined,
         epic_id: data.epic_id || undefined,
         discord_id: data.discord_id || undefined,
