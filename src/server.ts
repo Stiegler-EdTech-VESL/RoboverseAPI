5c29ba9889f3c0d2eca39ce00697eaf0e3fbd7c4
hectarek <hectarek13@gmail.com>
2023-11-13 14:56:20 -0500

    temporarily add localhost to cors

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,10 @@ import cors from "cors";
 
 const allowedOrigins = [
   "https://dev-robo.vesl.gg",
-  "https://dev-api.robo.vesl.gg/",
-  "https://prod-api.robo.vesl.gg/",
+  "https://dev-api.robo.vesl.gg",
+  "https://prod-api.robo.vesl.gg",
   "https://robo.vesl.gg", // Add additional origins here
+  "http://localhost:3000", // REMEMBER TO REMOVE
 ];
 
 //CORS stuff
